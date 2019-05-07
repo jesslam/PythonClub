6a972f0d5b13547a8742a9584ba63f67151129db
jesslam <49288633+jesslam@users.noreply.github.com>
2019-05-07 12:49:48 -0700

    Add files via upload

--- clubapp/views.py
+++ clubapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Event, Meeting, MeetingMinutes, Resource
 
 # Create your views here.
+def index(request):
+    return render(request, 'clubapp/index.html')
+
+def getResources(request):
+    resources_list=Resource.objects.all()
+    context={'resources_list' : resources_list}
+    return render(request, 'clubapp/resources.html', context=context)
+
+def getMeetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'clubapp/meetings.html', {'meeting_list' : meeting_list})
+
+def meetingDetails(request, id):
+    meet=get_object_or_404(MeetingMinutes, pk=id)
+    context = {
+        'meet' : meet,
+    }
+    return render(request, 'clubapp/meetingdetails.html', context=context)
