6ae30e8212d498bfdb96ae5e968a4260d2b4ceb9
jesslam <49288633+jesslam@users.noreply.github.com>
2019-05-12 23:38:50 -0700

    Delete urls.py

--- clubapp/clubapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('', views.index, name='index'),
-    path('getResources/', views.getResources, name='resources'),
-    path('getMeetings/', views.getMeetings, name='meetings'),
-    path('meetingDetails/<int:id>', views.meetingDetails, name='meetingdetails'),
-]
