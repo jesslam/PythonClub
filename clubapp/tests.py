0dbd1141c421b1c5c8001e4db38304757e790b57
jesslam <49288633+jesslam@users.noreply.github.com>
2019-05-14 11:54:15 -0700

    Add files via upload

--- clubapp/tests.py
+++ clubapp/tests.py
@@ -8,24 +8,24 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class MeetingTest(TestCase):
     def test_string(self):
-        type=Meeting(meetingtitle='hello')
-        self.assertEqual(str(type), type.meetingtitle)
+        m=Meeting(meetingtitle='hello')
+        self.assertEqual(str(m), m.meetingtitle)
     
     def test_table(self):
         self.assertEqual(str(Meeting._meta.db_table), 'meeting')
 
 class MeetingMinutesTest(TestCase):
     def test_string(self):
-        type=MeetingMinutes(durationid='1')
-        self.assertEqual(str(type), type.durationid)
+        mm=MeetingMinutes(durationid='1')
+        self.assertEqual(str(mm), mm.durationid)
 
     def test_table(self):  
         self.assertEqual(str(MeetingMinutes._meta.db_table), 'meetingminutes')
 
 class ResourceTest(TestCase):
     def test_string(self):
-        type=Resource(resourcename='Stack Overflow')
-        self.assertEqual(str(type), type.resourcename)
+        r=Resource(resourcename='Stack Overflow')
+        self.assertEqual(str(r), r.resourcename)
 
     def test_table(self):
         self.assertEqual(str(Resource._meta.db_table), 'resource')
@@ -44,7 +44,7 @@ class GetMeetingsTest(TestCase):
         
 
     def test_meetings_test_success(self):
-        response=self.client.get(reverse('meetings', args=(self.mevent.id)))
+        response=self.client.get(reverse('meetings'))
         self.assertEqual(response.status_code, 200)
 
 
